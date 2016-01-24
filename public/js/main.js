49154cf74adfeeff5c7ebaf4b256ebec9dbeefec
Pea Lutz <pea@glocal.coop>
2016-01-24 18:19:03 -0500

    Updated with new translation variables.

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,6 @@ var websiteText = {
       enter: "Enter",
       connect: "Connect",
       conference: "Conference",
-      mute: "Mute",
-      unmute: "Unmute",
       original: "Hear original language",
       interpretation: "Hear Interpretation",
       provide: "Provide Interpretation",
@@ -24,7 +22,17 @@ var websiteText = {
       salutation: "Hi",
       notepad: "Notepad",
       spreadsheet: "Spreadsheet",
-      chat: "Chat"
+      chat: "Chat",
+      channels: "Language Channels",
+      interpret: "Become Interpreter",
+      join_channel: "Join",
+      leave_channel: "Leave",
+      moderator: "Moderator",
+      queued: "Queued",
+      mute: "Mute",
+      unmute: "Unmute",
+      raise_hand: "Raise Hand",
+      lower_hand: "Lower Hand"
     },
     es: {
       username: "Ingrese su nombre",
@@ -32,8 +40,6 @@ var websiteText = {
       enter: "Ingrese",
       connect: "Connectarse",
       conference: "Sala De Conferencias",
-      mute: "Mícrófono innactivo",
-      unmute: "Activar micrófono",
       original: "Escuchar en idioma original",
       interpretation: "Escuchar la interpretación",
       provide: "Proporcionar interpretación",
@@ -46,7 +52,18 @@ var websiteText = {
       salutation: "Hola",
       notepad: "Notepad",
       spreadsheet: "Spreadsheet",
-      chat: "Chat"
+      chat: "Chat",
+      channels: "Language Channels",
+      interpret: "Become Interpreter",
+      join_channel: "Join",
+      leave_channel: "Leave",
+      moderator: "Moderator",
+      queued: "Queued",
+      mute: "Mícrófono innactivo",
+      unmute: "Activar micrófono",
+      raise_hand: "Raise Hand",
+      lower_hand: "Lower Hand"
+
     }
 };
 
@@ -432,7 +449,22 @@ $(function() {
         event.preventDefault()
 
     })
+
+    /**
+     * Activate Bootstrap tooltips
+     * This isn't working for dynamic elements
+     */
+    $.when.apply($, Views.RoomSidebar).done(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    /**
+     * Activate Clipboard
+     */
+    new Clipboard('.copy-link');
+
 });
+
 
 /**
  * Collaboration Functions
