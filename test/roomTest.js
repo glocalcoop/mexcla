48469052742e2e74acfb6778d2a9293c9427edf6
ziggy <ziggy@elephant-bird.net>
2016-02-04 16:30:38 -0500

    test runner and testing dir setup

---
+++ test/roomTest.js
@@ -0,0 +1,8 @@
+describe('test the test', function(){
+   it('should fail!', function(){
+       assert.equal(2, 1);
+   });
+  it('should pass', function(){
+    assert.equal('mexcla', 'mexcla');
+  });
+});
