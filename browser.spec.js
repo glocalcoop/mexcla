d8345a2c30743831719554976fccac751cbef06c
ziggy <ziggy@elephant-bird.net>
2016-02-07 00:29:28 -0500

    Testing pattern figured out
    Ok so the deal is: if you use wd.promiseChainRemote() you can return the
    browser object after each mocha it() block and you don't have to call
    notify(done) or tell mocha that it is async...it just does it! But watch
    out because mocha will pass the test as true without the return statement
    and you'll never know something was wrong.
    
    * const changed to var. Since there's no ES6 elsewhere in the project, I
    don't see any point in starting now.

--- browser.spec.js
+++ browser.spec.js
@@ -1,22 +1,23 @@
 // to run the tests, selenium-standalone must be installed and started:
 // selenium-standalone start
 
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 chai.should();
 
-const wd = require('wd');
+var wd = require('wd');
 // enables chai assertion chaining
 chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 
-const URL = 'localhost:8080/';
+var URL = 'localhost:8080/';
 
 // to run the tests, selenium-standalone must be installed and started:
 // selenium-standalone start
 
 
 describe('home page', function(){
+  this.timeout(10000);
   var browser;
 
   before(function(){
@@ -24,15 +25,27 @@ describe('home page', function(){
     return browser.init({browserName: 'chrome'});
   });
 
+  beforeEach(function(){
+    return browser.get(URL);
+  });
+
   after(function(){
     return browser.quit();
   });
 
   it('should have correct title', function(){
-    browser
-      .get(URL)
+    return browser
       .title().should.become('Mexcla Conferencing');
   });
+
+  it('should display register popup when create room is clicked', function(){
+    return browser
+      .elementById('create-new-room-button')
+      .click()
+      .elementById('register-modal')
+      .isDisplayed().should.become(true);
+  });
+
 });
 
 
