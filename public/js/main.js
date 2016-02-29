04e2ddcfb381da956527301494269da2e5d064d4
Pea Lutz <pea@glocal.coop>
2016-02-28 19:33:12 -0500

    Added app.audio = new Models.Audio() to make available to app.

--- public/js/main.js
+++ public/js/main.js
@@ -1168,6 +1168,7 @@ var MexclaRouter = Backbone.Router.extend({
 
 app.router = new MexclaRouter();
 // app.user = new Models.User();
+app.audio = new Models.Audio();
 app.user = null;
 
 Backbone.history.start(); // must call this to start router
--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,5 +1,6 @@
 app.router = new MexclaRouter();
 // app.user = new Models.User();
+app.audio = new Models.Audio();
 app.user = null;
 
 Backbone.history.start(); // must call this to start router
