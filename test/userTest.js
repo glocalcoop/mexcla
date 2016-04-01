4dad1ff293c570b1d6578219bdf18bedf9e3b9d9
ziggy <ziggy@elephant-bird.net>
2016-03-31 20:31:43 -0400

    Test for callOn & callOff

--- test/userTest.js
+++ test/userTest.js
@@ -21,11 +21,32 @@ describe('User', function(){
 
   describe('lowerHand', function(){
     
-    it('executes raise hand ajax call', function(){
+    it('executes lower hand ajax call', function(){
       new Models.User().lowerHand();
       $.ajax.getCall(1).args[0].url.should.eql('/room/id/123/lowerhand');
     });
     
+  });
+
+  describe('callOn', function(){
+    
+    it('executes call on ajax call', function(){
+      new Models.User().callOn("personid");
+      $.ajax.getCall(2).args[0].url.should.eql('/room/id/123/callon');
+      $.ajax.getCall(2).args[0].data._id.should.eql('personid');
+      
+    });
+
+  });
+
+  describe('callOff', function(){
+
+    it('executes call off ajax call', function(){
+      new Models.User().callOff("personid");
+      $.ajax.getCall(3).args[0].url.should.eql('/room/id/123/calloff');
+      $.ajax.getCall(3).args[0].data._id.should.eql('personid');
+      
+    });
   });
 
 });
