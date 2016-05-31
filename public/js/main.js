b309981ec8d58f441a47f2b3e548d3558b662e52
ziggy <ziggy@elephant-bird.net>
2016-05-31 16:47:47 -0400

    Create init function for app
    * this will make testing easier

--- public/js/main.js
+++ public/js/main.js
@@ -1447,11 +1447,12 @@ var MexclaRouter = Backbone.Router.extend({
   }
 });
 
-app.router = new MexclaRouter();
-// app.user = new Models.User();
-app.user = null;
-
-Backbone.history.start(); // must call this to start router
+app.init= function(){
+  app.router = new MexclaRouter();
+  // app.user = new Models.User();
+  app.user = null;
+  Backbone.history.start(); // must call this to start router
+};
 
 /**
  * UI Functions
--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,5 +1,6 @@
-app.router = new MexclaRouter();
-// app.user = new Models.User();
-app.user = null;
-
-Backbone.history.start(); // must call this to start router
+app.init= function(){
+  app.router = new MexclaRouter();
+  // app.user = new Models.User();
+  app.user = null;
+  Backbone.history.start(); // must call this to start router
+};
